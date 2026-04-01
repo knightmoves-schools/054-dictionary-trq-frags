4288ac8bec46f43522b617f1a304bcada94ee0c3
track-frags <jadirks@anamosa.k12.ia.us>
2026-04-01 15:24:40 +0000

    dd

--- Classroom.cs
+++ Classroom.cs
@@ -3,10 +3,6 @@
 public class Classroom{
 
     public Dictionary<int, string> CreateTeam(string first, string second, string third){
-        var team = new Dictionary<int, string>();
-        team.Add(1, first);
-        team.Add(2, second);
-        team.Add(3, third);
         return Directory<string first, string second, string third>();
     }
 }
